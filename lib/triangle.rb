6503aa728af2c5e6d229efbea9d2c0d94adae2cd
Jamal Gelin <gelin.jamal@gmail.com>
2019-01-25 10:02:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
    end
 
    def error
-      if (side1 < 0) || (side2 < 0) || (side3 < 0)
+      if (side1 = 0) || (side2 = 0) || (side3 = 0)
         self.kind = :TriangleError
       end
     end
